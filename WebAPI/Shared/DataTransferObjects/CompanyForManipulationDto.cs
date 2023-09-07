617818638704526badddba6e5fd6ffab21af5478
ABHISHEK\jilla <jilla.abhishek94@gmail.com>
2023-09-07 13:02:26 +0530

    Validation And Paging files

---
+++ WebAPI/Shared/DataTransferObjects/CompanyForManipulationDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObjects
+{
+    public abstract record CompanyForManipulationDto
+    {
+        [Required(ErrorMessage = "Company Name is Required.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string? Name { get; init; }
+        [Required(ErrorMessage = "Company Address is Required")]
+        public string? Address { get; init; }
+        [Required(ErrorMessage = "Company Country is Required")]
+        public string? Country { get; init; }
+        public IEnumerable<EmployeeForCreationDto>? Employees { get; init; }
+    }
+}
---
+++ WebAPI/Shared/DataTransferObjects/EmployeeForManipulationDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObjects
+{
+    public abstract record EmployeeForManipulationDto
+    {
+        [Required(ErrorMessage = "Employee name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string? Name { get; init; }
+
+        [Required(ErrorMessage = "Age is a required field")]
+        [Range(18, 40, ErrorMessage = "Age is required and it can't be lower than 18 and higher than 40.")]
+        public int Age { get; init; }
+        [Required(ErrorMessage = "Position is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20.")]
+        public string? Position { get; init; }
+    }
+}
